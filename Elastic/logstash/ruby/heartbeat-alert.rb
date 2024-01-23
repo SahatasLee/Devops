3d0951920f9782ea787e1905c7e42dbfbaa7b1bb
SAHATAS LEEVATHANA <sahatasnutlee@gmail.com>
2024-01-23 17:05:07 +0700

    heartbeat alert

---
+++ Elastic/logstash/ruby/heartbeat-alert.rb
@@ -0,0 +1,44 @@
+# custom_email_filter.rb
+require 'time'
+
+# Define a custom Logstash filter plugin
+def filter(event)
+    puts "Ruby version: #{RUBY_VERSION}"
+    t1 = event.get("@timestamp")
+    offset = 7 * 3600
+    event.set("new_timestamp", Time.at(t1 + offset))
+    # strftime auto +7
+    event.set("custom_timestamp", Time.at(t1 + 0).strftime("%d/%m/%Y, %H:%M:%S") ) 
+
+    puts "Service name:",event.get('[monitor][id]')
+
+    # Check if the event contains a 'service_name' field
+    if event.get('[monitor][id]')
+      service_name = event.get('[monitor][id]').downcase # Convert to lowercase for case-insensitive matching
+      
+      # Define a hash mapping service names to email addresses
+      service_email_mapping = {
+        # /Regex Pattern/ => 'email@email.com'
+        # /^ptp/ => 'ptp@gmail.com'
+        # /Regex Pattern/ => 'email-1@email.com, email-2@email.com',
+        # Add more service names and corresponding email addresses here
+        /^ptp/ => 'sahatasnutlee@gmail.com, sahataslee@outlook.co.th',
+        /^rtm/ => 'sahataslee@icloud.com'
+      }
+
+      # Find the matching email address
+      # Loops each service_email_mapping
+      service_email_mapping.each do |pattern, email|
+        # Check if the service name exists in the mapping
+        if service_name.match?(pattern)
+          # Set the 'email_sent' field to the corresponding email address
+          event.set('email_sent', email)
+          break
+        end
+      end
+    end
+  
+    # Return the modified event
+    return [event]
+  end
+  
